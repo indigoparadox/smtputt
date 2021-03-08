170202e5bdaad9a3f1842022773d67b13b6c8dd1
indigoparadox <indigo.repo@interfinitydynamics.info>
2021-03-08 15:37:54 -0500

    Moved smtputt service into own module.

--- smtputt/svc/__main__.py
+++ smtputt/svc/__main__.py
@@ -1,17 +1,15 @@
 
-from smtputt.smtpcache import SMTPCache
-import asyncore
 import logging
-import json
-import win32event
-import socket
-import win32service
 import sys
 import os
-#import pywin32types
-from win32serviceutil import ServiceFramework, HandleCommandLine
 from logging.handlers import NTEventLogHandler
-from threading import Thread
+
+import win32event
+import win32service
+from win32serviceutil import ServiceFramework, HandleCommandLine
+
+from smtputt import load_config
+from smtputt.server import SMTPuttServer
 
 # TODO: Dynamically determine this.
 CONFIG_PATH = 'c:\\smtputt-master\\src\\smtputt.ini'
@@ -25,17 +23,14 @@ class SMTPuttService( ServiceFramework ):
     def __init__( self, args ):
 
         ServiceFramework.__init__( self, args )
-        
+
         evthandler = NTEventLogHandler( 'SMTPutt' )
         logging.basicConfig( level=logging.INFO )
-        logger = logging.getLogger( '' )
-        logger.addHandler( evthandler )
+        logging.getLogger( '' ).addHandler( evthandler )
+        self.logger = logging.getLogger( 'svc' )
 
         self.hWaitStop = win32event.CreateEvent( None, 0, 0, None )
-        #self.overlapped = pywintypes.OVERLAPPED()
-        #self.overlapped.hEvent = win32event.CreateEvent( None, 0, 0, None )
-        #socket.setdefaulttimeout( 60 )
-        
+
         self.ReportServiceStatus( win32service.SERVICE_START_PENDING )
 
     @classmethod
@@ -47,44 +42,36 @@ class SMTPuttService( ServiceFramework ):
 
         try:
             self.smtp_cache.close()
-            #self.smtp_thread.join()
         except Exception as e:
-            logger.error( e )
+            self.logger.error( e )
 
         self.ReportServiceStatus( win32service.SERVICE_STOPPED )
         win32event.SetEvent( self.hWaitStop )
 
     def SvcDoRun( self ):
-        logger = logging.getLogger( 'smtpcache.svc' )
 
-        logger.info( 'using {} as config...'.format( CONFIG_PATH ) )
+        self.logger.info( 'using %s as config...', CONFIG_PATH )
 
-        config = configparser.ConfigParser()
-        config.read( CONFIG_PATH )
-        cfg_dict = dict( config.items( 'forwarder' ) )
+        server_cfg, module_cfgs = load_config( CONFIG_PATH )
 
-        self.smtp_cache = SMTPCache( **cfg_dict )
-
+        cache = SMTPuttServer( module_cfgs, **server_cfg )
+        cache_thread = cache.serve_thread()
         self.ReportServiceStatus( win32service.SERVICE_RUNNING )
-
-        logger.info(
-            'service intialized; sending mail to {} as {}; beginning loop'.format(
-            config['forwarder']['remoteserver'], config['forwarder']['fromaddress'] ) )
-        
-        #self.smtp_thread = Thread( target=asyncore.loop, kwargs={'timeout': 1} )
-        #self.smtp_thread.start()
-        asyncore.loop()
+        cache_thread.join()
 
         servicemanager.LogMsg(
             servicemanager.EVENTLOG_INFORMATION_TYPE,
             servicemanager.PYS_SERVICE_STOPPED,
             ('SMTPutt', '') )
 
-if '__main__' == __name__:
+def main():
+
     if len( sys.argv ) == 1:
         servicemanager.Initialize()
         servicemanager.PrepareToHostSingle( SMTPuttService )
         servicemanager.StartServiceCtrlDispatcher()
     else:
         SMTPuttService.handle_cmd()
-    
+
+if '__main__' == __name__:
+    main()
